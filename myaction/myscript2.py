6bbe8f4d24b53997b7eb0627287c5c6556716079
ikeikeikeike <jp.ne.co.jp@gmail.com>
2021-06-07 18:00:23 +0900

    action

---
+++ myaction/myscript2.py
@@ -0,0 +1,17 @@
+def some():
+    """myaction
+    ```python
+    a = None
+    b = None
+    c = None
+    d = None
+    e = None
+    f = None
+    g = None
+    h = None
+    i = None
+    j = None
+    k = None
+    ```
+    """
+    return None
